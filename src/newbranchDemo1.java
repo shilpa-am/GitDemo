556955443534154e7b30e5a667d81800c0f8bf8d
shilpa <shilpaam55@gmail.com>
2024-03-26 18:46:13 +0530

    Architect Asian develop

---
+++ src/newbranchDemo1.java
@@ -0,0 +1,12 @@
+
+public class newbranchDemo1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("GitDemo freshcode");
+		System.out.println("GitDemo 1");
+		System.out.println("GitDemo 2");
+		System.out.println("GitDemo 3");
+	}
+
+}
