541daf710543ac2ada63df842bc03083d4491300
shilpa <shilpaam55@gmail.com>
2024-03-26 18:54:26 +0530

    gitx changes to master brnach

---
+++ src/Demonewbranch2.java
@@ -0,0 +1,11 @@
+
+public class Demonewbranch2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Gitnewbranch 1");
+		System.out.println("Gitnewbranch 2");
+		System.out.println("Gitnewbranch 3");
+	}
+
+}
