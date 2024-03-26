ac4689521dd8b61c5290d56bf80ea938ba9335ae
shilpa <shilpaam55@gmail.com>
2024-03-26 18:03:14 +0530

    gitx changes in demo2

--- src/Demo2.java
+++ src/Demo2.java
@@ -4,6 +4,9 @@ public class Demo2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("GitDemo freshcode");
+		System.out.println("Gitx 1");
+		System.out.println("Gitx 2");
+		System.out.println("Gitx 3");
 	}
 
 }
