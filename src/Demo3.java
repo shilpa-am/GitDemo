f6c82471d94f513950794d8dec288146c096e12f
shilpa <shilpaam55@gmail.com>
2024-03-27 07:17:10 +0530

    American guy code

---
+++ src/Demo3.java
@@ -0,0 +1,13 @@
+
+public class Demo3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("GitDemo freshcode");
+		System.out.println("Gitx 1");
+		System.out.println("Gitx 2");
+		System.out.println("Gitx 3");
+		System.out.println("Gitx brnach check");
+	}
+
+}
