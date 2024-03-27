5aebf9b86519ca54609b2a29dc46591e0a9fe085
shilpa <shilpaam55@gmail.com>
2024-03-27 10:13:36 +0530

    gitx code

---
+++ src/gitxfile1.java
@@ -0,0 +1,12 @@
+
+public class gitxfile1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("GitDemo freshcode");
+		System.out.println("Gitx 1");
+		System.out.println("Gitx 2");
+		System.out.println("Gitx 3");
+	}
+
+}
